7a2f0ac224efb3141f16a1dc9bcbeceaa233d840
prestokp <prestokp@rose-hulman.edu>
2019-02-06 11:37:07 -0500

    Test function for raise arm
    Test function executed
    -KP

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -17,16 +17,20 @@ def main():
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
     run_test_arm()
+    run_test_lower_arm()
+    run_test_calibrate()
 
 def run_test_arm():
     robot = rosebot.RoseBot()
     robot.arm_and_claw.raise_arm()
 
-#def run_test_lower_arm():
-    #robot = rosebot.RoseBot()
-    #robot.arm_and_claw.lower_arm()
+def run_test_lower_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.lower_arm()
 
-#def run_test_calibrate():
+def run_test_calibrate():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
 
 
 
