39bfc038d3b5c370a8c5f033cad87b346c267c04
prestokp <prestokp@rose-hulman.edu>
2019-02-10 16:32:40 -0500

    Shared Gui Delegate Function Local Commits
    KP Responsible for shared Gui delegate drive system
    -KP

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -23,9 +23,24 @@ class Handler(object):
         self.robot.drive_system.go(int(left_wheel_speed*-1), int(right_wheel_speed*-1))
 
     def left(self, left_wheel_speed, right_wheel_speed):
-        left_wheel_speed = left_wheel_speed * -1
+        print("going left", left_wheel_speed, right_wheel_speed)
+        self.robot.drive_system.go((int(left_wheel_speed))*-1, int(right_wheel_speed)*-1)
+
+    def right(self, left_wheel_speed, right_wheel_speed):
+        print("going right", left_wheel_speed, right_wheel_speed)
+        self.robot.drive_system.go(int(left_wheel_speed), int(right_wheel_speed)*-1)
 
     def stop(self):
-        print('stop')
+        print('stopping...')
         self.robot.drive_system.stop()
-        
+
+    def go_straight_for_seconds(self, left_wheel_speed, right_wheel_speed, seconds):
+        print("going for seconds", left_wheel_speed, right_wheel_speed, seconds)
+        while start < seconds:
+            self.forward(int(right_wheel_speed), int(left_wheel_speed))
+            break
+        self.stop()
+
+    def
+
+
