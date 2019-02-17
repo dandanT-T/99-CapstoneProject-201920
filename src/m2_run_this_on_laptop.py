7fb83125f50d646331819a4d3b2d3353622594b5
xiny <xiny@rose-hulman.edu>
2019-02-17 00:40:13 -0500

    new frame

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -54,6 +54,7 @@ def main():
     tones_frame=make_higher_tones_frame(main_frame,mqtt_sender)
     camera_frame=get_camera_frame(main_frame,mqtt_sender)
     random_frame=get_random_frame(main_frame,mqtt_sender)
+    color_and_grab_frame=get_grabbing_color_frame(main_frame,mqtt_sender)
 
     # -------------------------------------------------------------------------
     # Grid the frames.
@@ -61,6 +62,7 @@ def main():
     tones_frame.grid(row=1, column=1)
     camera_frame.grid(row=2,column=1)
     random_frame.grid(row=0,column=2)
+    color_and_grab_frame.grid(row=1,column=2)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -98,9 +100,9 @@ def make_higher_tones_frame(main_frame,mqtt_sender):
     Make a frame about individual frame in feature 9.
     The robot should make tones as it moves, with the tones increasing in frequency as the robot gets closer to the
     object. The user should be able to set the initial and rate of increase of the frequencies via the GUI.
-    :param main_frame:
-    :param mqtt_sender:
-    :return: frame
+    :param main_frame: ttk.Frame
+    :param mqtt_sender:mqtt_sender
+    :return: ttk.frame
     '''
     make_tone_frame = ttk.Frame(main_frame, padding=10, borderwidth=5, relief="ridge")
     make_tone_frame.grid()
@@ -140,7 +142,7 @@ def get_camera_frame(window,mqtt_sender):
     provide the camera frame for the GUI
     :param window: ttk.frame
     :param mqtt_sender: mqtt_sender
-    :return: None
+    :return: ttk.Frame
     '''
     frame=ttk.Frame(window,padding=10, borderwidth=5, relief="ridge")
     frame.grid()
@@ -187,7 +189,7 @@ def get_random_frame(window,mqtt_sender):
     establish a new frame for random functions
     :param window: ttk.frame
     :param mqtt_sender: mqtt_sender
-    :return: None
+    :return: ttk.Frame
     '''
     main_frame=ttk.Frame(window,padding=10, borderwidth=5, relief="ridge")
     main_frame.grid()
@@ -209,6 +211,76 @@ def handle_random_functions(mqtt_sender):
     print("doing random things")
     a=random.randint(1,10)
     mqtt_sender.send_message("m2_random_functions",[a])
+
+def get_grabbing_color_frame(window,mqtt_sender):
+    '''
+    a frame for the function color_and_grab. This function is used as recognizing certain color and grab that object
+    :param window: ttk.Frame
+    :param mqtt_sender: mqtt_sender
+    :return: ttk.frame
+    '''
+    frame=ttk.Frame(window,padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
+    frame_label=ttk.Label(frame,text="Grab certain an object with color")
+    frame_label.grid(row=0,column=1)
+
+    color_label=ttk.Label(frame,text="Color(need to be an int)")
+    color_label.grid(row=1,column=0)
+    color_entry=ttk.Entry(frame,width=9)
+    color_entry.grid(row=2,column=0)
+    color_and_grab_button=ttk.Button(frame,text="Find the object and grab it")
+    color_and_grab_button.grid(row=2,column=2)
+
+    list_of_colors_label=ttk.Label(frame,text="Translation between colors and ints:")
+    list_of_colors_label.grid(row=3,column=0)
+
+    first_color_label=ttk.Label(frame,text="No Color")
+    first_int_label=ttk.Label(frame,text="0")
+    first_color_label.grid(row=4,column=0)
+    first_int_label.grid(row=4,column=1)
+
+    second_color_label = ttk.Label(frame, text="Black")
+    second_int_label = ttk.Label(frame, text="1")
+    second_color_label.grid(row=5, column=0)
+    second_int_label.grid(row=5, column=1)
+
+    third_color_label = ttk.Label(frame, text="Blue")
+    third_int_label = ttk.Label(frame, text="2")
+    third_color_label.grid(row=6, column=0)
+    third_int_label.grid(row=6, column=1)
+
+    fourth_color_label = ttk.Label(frame, text="Green")
+    fourth_int_label = ttk.Label(frame, text="3")
+    fourth_color_label.grid(row=7, column=0)
+    fourth_int_label.grid(row=7, column=1)
+
+    fifth_color_label = ttk.Label(frame, text="Yellow")
+    fifth_int_label = ttk.Label(frame, text="4")
+    fifth_color_label.grid(row=8, column=0)
+    fifth_int_label.grid(row=8, column=1)
+
+    sixth_color_label = ttk.Label(frame, text="Red")
+    sixth_int_label = ttk.Label(frame, text="5")
+    sixth_color_label.grid(row=9, column=0)
+    sixth_int_label.grid(row=9, column=1)
+
+    seventh_color_label = ttk.Label(frame, text="White")
+    seventh_int_label = ttk.Label(frame, text="6")
+    seventh_color_label.grid(row=10, column=0)
+    seventh_int_label.grid(row=10, column=1)
+
+    seventh_color_label = ttk.Label(frame, text="Brown")
+    seventh_int_label = ttk.Label(frame, text="7")
+    seventh_color_label.grid(row=11, column=0)
+    seventh_int_label.grid(row=11, column=1)
+
+    color_and_grab_button["command"]=lambda :handle_color_and_grab(color_entry.get(),mqtt_sender)
+
+    return frame
+
+def handle_color_and_grab(color,mqtt_sender):
+    print(color)
+    mqtt_sender.send_message("m2_find_color_and_grab",[color])
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
