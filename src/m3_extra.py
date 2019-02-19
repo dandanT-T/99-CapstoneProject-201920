dfbc2d653891282759b375788ddb5a4e9f542062
prestokp <prestokp@rose-hulman.edu>
2019-02-19 13:13:55 -0500

    Capstone Sprint 3
    -KP

--- src/m3_extra.py
+++ src/m3_extra.py
@@ -19,23 +19,28 @@ def m3_start_game(robot, parameter_box):
     while True:
         if parameter_box.need_to_start == True:
             break
+    while True:
+        while True:
+            if robot.sensor_system.color_sensor.get_reflected_light_intensity()>20:
+                break
+        m3_step_one(robot)
 
+def m3_step_one(robot):
     k = time.time()
     while True:
         print(robot.sensor_system.color_sensor.get_reflected_light_intensity())
-        if robot.sensor_system.color_sensor.get_reflected_light_intensity() < 10: #parameter_box.color_threshold:  ### Might be a redundant conditional statement
+        if robot.sensor_system.color_sensor.get_reflected_light_intensity() < 10:  # parameter_box.color_threshold:  ### Might be a redundant conditional statement
             for k in range(2):
                 robot.sound_system.beeper.beep().wait()
             print('You win')
             robot.drive_system.stop()
             break
-        if time.time() - k > 100:
+        if time.time() - k > 60:
             print('You Lose')
             robot.drive_system.stop()
-
+            break
 
         time.sleep(0.01)
-
 
 
 
