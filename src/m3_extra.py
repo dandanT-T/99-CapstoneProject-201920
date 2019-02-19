ac213ab02000a3b473947cddcd33595910b67f71
prestokp <prestokp@rose-hulman.edu>
2019-02-19 13:34:22 -0500

    Capstone Sprint 3
    -KP

--- src/m3_extra.py
+++ src/m3_extra.py
@@ -14,7 +14,7 @@ class Game(object):
 
 
 def m3_start_game(robot, parameter_box):
-    robot = rosebot.RoseBot()
+    #robot = rosebot.RoseBot()
     #print('start')
     while True:
         if parameter_box.need_to_start == True:
@@ -28,7 +28,7 @@ def m3_start_game(robot, parameter_box):
 def m3_step_one(robot):
     k = time.time()
     while True:
-        print(robot.sensor_system.color_sensor.get_reflected_light_intensity())
+        #print(robot.sensor_system.color_sensor.get_reflected_light_intensity())
         if robot.sensor_system.color_sensor.get_reflected_light_intensity() < 10:  # parameter_box.color_threshold:  ### Might be a redundant conditional statement
             for k in range(5):
                 robot.sound_system.beeper.beep().wait()
@@ -36,11 +36,39 @@ def m3_step_one(robot):
             robot.sound_system.tone_maker.play_tone_sequence([
                 (392, 350, 100), (392, 350, 100), (392, 350, 100), (311.1, 250, 100),
                 (466.2, 25, 100), (392, 350, 100), (311.1, 250, 100), (466.2, 25, 100),
-                (392, 700, 100), (587.32, 350, 100), (587.32, 350, 100)])
+                (392, 700, 100)])
             break
-        if time.time() - k > 10:
+        if time.time() - k > 20:
             print('You Lose')
             robot.drive_system.stop()
+            robot.sound_system.tone_maker.play_tone_sequence([
+                (392, 350, 100), (392, 350, 100), (392, 350, 100), (311.1, 250, 100),
+                (466.2, 25, 100), (392, 350, 100), (311.1, 250, 100), (466.2, 25, 100),
+                (392, 700, 100), (587.32, 350, 100), (587.32, 350, 100),
+                (587.32, 350, 100), (622.26, 250, 100), (466.2, 25, 100),
+                (369.99, 350, 100), (311.1, 250, 100), (466.2, 25, 100),
+                (392, 700, 100),
+                (784, 350, 100), (392, 250, 100), (392, 25, 100), (784, 350, 100),
+                (739.98, 250, 100), (698.46, 25, 100), (659.26, 25, 100),
+                (622.26, 25, 100), (659.26, 50, 400), (415.3, 25, 200),
+                (554.36, 350, 100),
+                (523.25, 250, 100), (493.88, 25, 100), (466.16, 25, 100),
+                (440, 25, 100),
+                (466.16, 50, 400), (311.13, 25, 200), (369.99, 350, 100),
+                (311.13, 250, 100), (392, 25, 100), (466.16, 350, 100),
+                (392, 250, 100),
+                (466.16, 25, 100), (587.32, 700, 100), (784, 350, 100),
+                (392, 250, 100),
+                (392, 25, 100), (784, 350, 100), (739.98, 250, 100),
+                (698.46, 25, 100),
+                (659.26, 25, 100), (622.26, 25, 100), (659.26, 50, 400),
+                (415.3, 25, 200),
+                (554.36, 350, 100), (523.25, 250, 100), (493.88, 25, 100),
+                (466.16, 25, 100), (440, 25, 100), (466.16, 50, 400),
+                (311.13, 25, 200),
+                (392, 350, 100), (311.13, 250, 100), (466.16, 25, 100),
+                (392.00, 300, 150), (311.13, 250, 100), (466.16, 25, 100), (392, 700)
+                ]).wait()
             break
 
         time.sleep(0.01)
